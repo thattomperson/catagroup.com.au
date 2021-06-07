6bb171a426a3c23ca57d7bc63e96313affd786d8
Tom Albrighton <thomas.albrighton@four.io>
2021-06-07 20:30:45 +0930

    New Site

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Header from '$lib/Header/index.svelte';
+	import Footer from '$lib/Footer.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
+
+<style>
+	@tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/routes/products/index.json.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import type { Locals } from '$lib/types';
+
+// GET /todos.json
+export const get: RequestHandler<Locals> = async (request) => {
+  return {
+    body: [
+      {
+        'name': 'Gobius',
+        'image': 'https://catagroup.com.au/216e2b3213d7435089695ef26c9f02b1.png',
+        'description': 'An awesome product',
+        'link': 'https://www.gobiuspro.com/',
+      },{
+        'name': 'Kova Pump',
+        'image': 'https://catagroup.com.au/877d87c5741265e86727669c6985e1d0.png',
+        'description': 'An awesome product',
+        'link': 'https://www.kovapumps.com.au/',
+      },{
+        'name': 'Master Vent',
+        'image': 'https://catagroup.com.au/622bcf21408eb097cfdd8fc0b884fdd6.png',
+        'description': 'An awesome product',
+        'link': 'https://www.catagroup.com.au/p/master-vent',
+      },{
+        'name': 'Arctic Steel',
+        'image': 'https://catagroup.com.au/9bdf43ce36a6e3fd5dd941c31751532b.png',
+        'description': 'An awesome product',
+        'link': 'https://arcticsteel.com.au/',
+      },
+    ]
+  }
+};
