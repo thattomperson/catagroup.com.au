c615bfd7cd3a9638a8b4501e26a4c2ce312128cf
Tom Albrighton <thomas.albrighton@four.io>
2021-08-26 17:17:45 +0930

    Use the vercel builder

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +10,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({
+    adapter: vercel({
       // default options are shown
       pages: '.vercel_build_output',
       assets: '.vercel_build_output',
