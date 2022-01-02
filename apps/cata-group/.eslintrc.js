5e4152894c64248c56ecf4c56169032028743201
Tom Albrighton <thomas.albrighton@four.io>
2022-01-03 00:44:57 +1030

    renamed packages again

--- apps/cata-group/.eslintrc.js
+++ apps/cata-group/.eslintrc.js
@@ -1,1 +1,1 @@
-module.exports = require("@catagroup/config/eslint-preset");
+module.exports = require("config/eslint-preset");
--- apps/kovapumps/next.config.js
+++ apps/kovapumps/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["@catagroup/ui"]);
+const withTM = require("next-transpile-modules")(["ui"]);
 const path = require("path")
 
 module.exports = withTM({
