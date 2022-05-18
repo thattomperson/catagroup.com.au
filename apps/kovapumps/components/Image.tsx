7716df30a7d2930ea6fe5c4a8c41a10b35da0507
Tom Albrighton <thomas.albrighton@four.io>
2022-05-18 14:49:12 +1000

    Remove empty component

--- apps/kovapumps/components/Image.tsx
+++
@@ -1,5 +0,0 @@
-// import { default as NextImage } from 'next/image';
-
-// export default function Image({ image, maxWidth, maxHeight, ...props }) {
-//   return <NextImage src={image.url} />
-// }
