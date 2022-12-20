275b666b3f325313638d4a7903200910165522e5
Tom Albrighton <thomas.albrighton@four.io>
2022-12-20 16:21:14 +1030

    Fix incorrect url

--- apps/kovapumps/lib/api/index.ts
+++ apps/kovapumps/lib/api/index.ts
@@ -1,7 +1,7 @@
 import { getSdk, SdkFunctionWrapper } from "./generated/graphql";
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient(process.env.GRAPHCMS_PROJECT_API)
+export const client = new GraphQLClient(process.env.HYGRAPH_PROJECT_API);
 
 const authWrapper: SdkFunctionWrapper = async (action, _operationName) => {
   return action({
